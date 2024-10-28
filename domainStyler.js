2318eaa936e8768f15e5a7912737cd132a48840f
George Flood <georgeflood@hydro-cleansing.com>
2024-10-28 16:33:18 +0000

    added images to flood response urls

--- domainStyler.js
+++ domainStyler.js
@@ -16,8 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
           domainConfig.text;
       } else {
         //// DEFAULT BACKGROUND IMAGE AND TEXT
-        container.style.backgroundImage =
-          'url(https://images.unsplash.com/photo-1553211274-94febfa26133?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+        container.style.backgroundImage = 'url(/images/FloodResponse.jpg)';
         document.querySelector('.contactForm--headers h3').textContent =
           'Are you interested in working together?';
       }
