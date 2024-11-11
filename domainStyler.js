77edf86d38b6bd9c050f70b97e9d31fdfbe67dd5
George Flood <georgeflood@hydro-cleansing.com>
2024-11-11 14:54:20 +0000

    dynamic title

--- domainStyler.js
+++ domainStyler.js
@@ -8,6 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
       const container = document.querySelector('.background--container');
       let backgroundImageUrl;
 
+      const title = hostName
+        .replace(/\.(co\.uk|com|uk|org\.uk|net)$/, '') // Remove domain extensions
+        .replace(/[-_]/g, ' ') // Replace hyphens and underscores with spaces
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter of each word
+        .join(' ');
+
+      document.title = `${title} | Contact Us`;
+
       if (data[hostName]) {
         const domainConfig = data[hostName];
         backgroundImageUrl = domainConfig.backgroundImage;
